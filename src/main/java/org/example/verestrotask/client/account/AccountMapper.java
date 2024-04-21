684c03b0738cf8847a7124565ff8805f29a1a9f4
Łukasz Bąk <lukasz.bak@interiowy.pl>
2024-04-21 15:14:16 +0200

    Unique account

--- src/main/java/org/example/verestrotask/client/account/AccountMapper.java
+++ src/main/java/org/example/verestrotask/client/account/AccountMapper.java
@@ -6,20 +6,26 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Optional;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Service
 public class AccountMapper {
+    private final AccountRepository accountRepository;
+
+    public AccountMapper(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
 
     Account dtoToEntity(AccountDto dto) {
         Account account = new Account();
         account.setIdentifier(identifier());
         BigDecimal bigDecimal = enumToBigDecimal(dto.balance());
         account.setBalance(bigDecimal);
-        int DAT_LIMIT = 3;
-        account.setDayLimit(DAT_LIMIT);
+        int dayLimit = 3;
+        account.setDayLimit(dayLimit);
         return account;
     }
 
@@ -29,8 +35,14 @@ public class AccountMapper {
 
     private BigInteger identifier() {
         Random random = new Random();
-        String collect = IntStream.range(0, 20).mapToObj(i -> Integer.toString(random.nextInt(10))).collect(Collectors.joining());
-        return new BigInteger(collect);
+        String randomAccount = IntStream.range(0, 20).mapToObj(i -> Integer.toString(random.nextInt(10))).collect(Collectors.joining());
+        BigInteger account = new BigInteger(randomAccount);
+
+        Optional<Account> byIdentifier = accountRepository.findByIdentifier(account);
+        if (byIdentifier.isPresent()) {
+            return identifier();
+        }
+        return new BigInteger(randomAccount);
     }
 
 
